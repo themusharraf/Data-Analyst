7deb78d37bd73715adc396bb2792f043258812cd
allnc <meibrohimov@gmail.com>
2023-12-08 22:53:44 +0500

    lesson py basic

--- main.py
+++ main.py
@@ -6,8 +6,12 @@ my_list = list(range(100_000))  # python list 0~99999 --> Normal
 my_array = np.array(range(100_000))  # numpy array (massiv) 0~99999 --> Vektorlashgan
 
 start = time.time()
-for _ in range(10): my_array * 2
+for _ in range(10):
+    my_array * 2
 end = time.time()
+
+# 0.013599872589111328
+# 0.0010001659393310547
 
 natija = end - start
 print("Dastur sekund oladi: ", natija)
